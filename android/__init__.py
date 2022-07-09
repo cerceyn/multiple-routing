beb6b3539d582fcf0f22cc874d2288aedffc0e59
berce <104266528+cerceyn@users.noreply.github.com>
2022-07-09 23:29:15 +0300

    first blood

---
+++ android/__init__.py
@@ -0,0 +1,146 @@
+from subprocess import PIPE, Popen
+def pip_(module):
+    onemli(f"installing {module} for cerceynlab")
+    pip_cmd = ["pip", "install", f"{module}"]
+    process = Popen(pip_cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
+from time import sleep as antripp
+from .clabtoken import CLabToken
+try: 
+    from rich.console import Console
+    from rich.panel import Panel
+except:
+    pip_("rich")
+finally:
+    from rich.console import Console
+    from rich.panel import Panel
+import os, shutil
+import sys
+
+console = Console()
+def nn():
+    console.print("\n\n")
+def hata (text):
+    nn()
+    console.log(f'[bold red]❌ {text}[/]') 
+    sys.exit()
+def pip_(module):
+    onemli(f"📥 installing {module} for cerceynlab")
+    pip_cmd = ["pip", "install", f"{module}"]
+    process = Popen(pip_cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
+def myscript ():
+    return "ZnJvbSByaWNoLmNvbnNvbGUgaW1wb3J0IENvbnNvbGUNCmZyb20gcmljaC5wYW5lbCBpbXBvcnQgUGFuZWwNCg0KaW1wb3J0IHN5cw0KY29uc29sZSA9IENvbnNvbGUoKQ0KZGVmIGJpbGdpICh0ZXh0KToNCiAgICBjb25zb2xlLnByaW50KFBhbmVsKGYnW2JsdWVde3RleHR9Wy9dJyx3aWR0aD03MCksanVzdGlmeT0iY2VudGVyIikgIA0KZGVmIHNvcnUgKHNvcnUpOg0KICAgIGNvbnNvbGUucHJpbnQoUGFuZWwoZidbYm9sZCB5ZWxsb3dde3NvcnV9Wy9dJyx3aWR0aD03MCksanVzdGlmeT0iY2VudGVyIikgICAgICAgICAgICAgICAgICAgICAgICAgDQogICAgcmV0dXJuIGNvbnNvbGUuaW5wdXQoZiJbYm9sZCB5ZWxsb3ddPj4gWy9dIikNCmRlZiBoYXRhICh0ZXh0KToNCiAgICBjb25zb2xlLnByaW50KFBhbmVsKGYnW2JvbGQgcmVkXXt0ZXh0fVsvXScsd2lkdGg9NzApLGp1c3RpZnk9ImNlbnRlciIpICAgIA0KICAgIHN5cy5leGl0KCkNCg0KYmlsZ2koIlBhc3N3b3JkIGRlY29kaW5nLi4uIikNCmRvZ3J1cGFzcz0gNjg5Nw0KICAgIA0Kc2lmcmUgPSBzb3J1KCJNZXJoYWJhISDFnmlmcmU6IikNCnRyeToNCiAgICBkb2dydXBhc3M9IGludChkb2dydXBhc3MpDQogICAgaWYgaW50KHNpZnJlKSAhPSBkb2dydXBhc3M6DQogICAgICAgIGhhdGEoIllhbmzEscWfIMWfaWZyZSIpDQpleGNlcHQgVHlwZUVycm9yOg0KICAgIGhhdGEoIllhbmzEscWfIMWfaWZyZSIpDQpleGNlcHQgRXhjZXB0aW9uIGFzIGU6DQogICAgaGF0YSgiSGF0YTogIitzdHIoZSkp"                 
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    with console.status("[bold blue] İnternet bağlantınız kontrol ediliyor...") as status:
+        try:
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+            console.log("[green] Bağlantı var gibi görünüyor! [/green]")
+            return True
+        except socket.error as ex:
+            print(ex)
+            console.log("[red] Bağlantı yok gibi gözüküyor ! [/red]")
+            return False                        
+def bilgi (text):
+    nn()
+    console.log(f'[blue]ℹ️ {text}[/]')
+def clabtoken(text,coz=True):
+    data = [1, 2, 3, 4, 5]
+    ktext=None
+    key=None
+    nn()
+    with console.status("[bold blue] Clabtoken İşlemi Sürüyor...") as status:
+        while data:
+            num = data.pop(0)
+            antripp(2)
+            if num==1:
+                console.log(f"[green] Şifrelenmiş keyler ayrıştırılıyor...[/green]")
+                try:
+                    ktext=text.split('&&')[1]
+                    key=text.split('&&')[2]
+                except IndexError:
+                    hata("Bu bir CLab-AccountToken değil!")
+            elif num==2:
+                test_crpt = CLabToken()
+                console.log(f"[green]Token nesnesi oluşturuldu![/green]")
+            elif num==3 and coz==False:
+                test_enctext = test_crpt.yap(ktext, key)
+                console.log(f"[green]Token Şifreleniyor.[/green]")
+                antripp(2)
+                test_enc_text = f"CLab&&{test_enctext}&&{key}"
+                console.log(f"[green]Token Formatı Ayarlandı![/green]")
+            elif num==4 and coz:
+                console.log(f"[green]Token çözülüyor..[/green]")
+                test_dec_text = test_crpt.coz(ktext, key)
+                console.log(f"[green]Bilgiler ayrıştırılıyor...[/green]")
+                antripp(2)
+                api_id = test_dec_text.split("|")[0]
+                api_hash = test_dec_text.split("|")[1]
+                string = test_dec_text.split("|")[2]
+            elif num==5:
+                if not coz:
+                    console.log(f"[green]Token oluşturma işlemi başarılı![/green]")
+                    return test_enc_text
+                else:
+                    console.log(f"[green]Token çözme işlemi başarılı![/green]")
+                    return api_id, api_hash, string 
+
+    try:
+        ss = text.split('|')
+        if len(ss[1]) <29:
+            hata("Bu bir CLab-AccountToken değil!")
+        return ss[2], ss[1], ss[3]
+    except IndexError:
+        hata("Bu bir CLab-AccountToken değil!")
+    return None, None, None
+def passed (text):
+    console.print("\n")
+    console.print(Panel(f'[steel_blue1]🚸 {text}[/]',width=70),justify="center")
+def noadded (text):
+    nn()
+    console.log(f'[red]❎ {text}[/]')  
+def basarili (text):
+    nn()
+    console.log(f'[bold green]✅ {text}[/]')                         
+def onemli (text):
+    nn()
+    console.print(f'[bold cyan]❗ {text}[/]')      
+def ads (text,time=5):
+    nn()
+    console.log(f'[green]🍔 {text}[/]')     
+    antripp(time)              
+def soru (soru):
+    nn()
+    console.print(f'[bold thistle1]❔ {soru}[/]')
+    try:                 
+        return console.input(f"[bold yellow1]>> [/]")
+    except KeyboardInterrupt:
+        hata("Klavye çıkışı yapıldı!")
+def soru_ (soru):
+    nn()
+    console.print(f'[bold thistle1]❔ {soru}[/]')
+    return console.input(f"[bold yellow1]>> [/]")
+
+def onay (text):
+    while True:
+        cevap=soru(text)
+        if cevap in ["Evet","evet","Yes","yes","Y","y"]:
+            return True
+        elif cevap in ["Hayır","Hayır","hayır","hayir","No","no"]:
+            return False
+        else:
+            noadded("Lütfen sadece evet-yes veya hayır-no diyin!")
+def logo (satirbırak=False):
+    text = "█▀▀ █▀▀ █▀█ █▀▀ █▀▀ █▄█ █▄░█\n█▄▄ ██▄ █▀▄ █▄▄ ██▄ ░█░ █░▀█\n\n█░░ ▄▀█ █▄▄\n█▄▄ █▀█ █▄█"
+    if satirbırak:
+        for i in range(25):
+            console.print("\n")
+    console.print(Panel(f'[bold medium_purple]{text}[/]',width=90),justify="center")
---
+++ android/__main__.py
@@ -0,0 +1,310 @@
+from traceback import format_exc
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.functions.messages import AddChatUserRequest
+from .events import register as clabtetikleyici 
+from telethon.events import NewMessage as bberc
+from telethon.errors import PeerIdInvalidError
+from telethon.sessions import StringSession
+from telethon import TelegramClient
+from subprocess import PIPE, Popen
+from rich import print as rprint
+from telethon import types
+from time import sleep
+from android import *
+import asyncio, os
+import base64
+
+loop = asyncio.get_event_loop()
+
+def n():
+    console.print("\n")
+def log(text,renk=None):
+    if renk:
+        console.log(f"[{renk}]{text}[/{renk}]")
+    else:
+        console.log(f"{text}")
+Token="MTc4Mzc1MjY5ODpBQUUzajlkQ0toSW9kZU5GSTZ3aHEtYkFTd3lPWHBLWXgtWQ=="
+try:
+    bot = TelegramClient('bot',api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+except Exception:
+    hata(f"Bir sorunla karşılaştık! Bu hatayı geliştiriciye bildirin:\n{format_exc()}")
+async def botagir():
+    global bot
+    data = [1,2,3,4]
+    n()
+    with console.status("[bold blue] Bota girme işlemi sürüyor...") as status:
+        while data:
+            num = data.pop(0)
+            sleep(.5)
+            if num==1:
+                log("🔑 Token ayarlanıyor...","cyan")
+                token = base64.b64decode(Token)
+            elif num==2:
+                console.log("[cyan] 🎟️ Giriş yapılıyor...[/cyan]")
+                console.log("[red] 🎟️ Hata alınması en muhtemel yer...[/red]")
+                await bot.start(bot_token=token)
+                bot.parse_mode="html"
+            elif num==3:
+                try:
+                    await bot.send_message(0000000,"⌛ Bot başladı!")
+                except:
+                    noadded('Mesaj gönderilememe hatası!')
+            elif num==4:
+                console.log(f'[bold][green]✅ Bot girişi yapıldı!')
+                #await bot.disconnect()
+    return bot
+async def setdirectory(pprint=True):
+    sep = os.sep
+    li = os.getcwd().split(sep)
+    if pprint:bilgi(li[-1])
+    passed("Dizin ayarlanıyor")
+    if os.name=="nt":
+        os.chdir("c://")
+        li = os.getcwd().split(sep)
+    elif "home" in li: #termux
+        while True:
+            if not li[-1] == "home": #termux
+                os.chdir(os.pardir)
+            else:
+                break
+            li = os.getcwd().split(sep)
+    else:
+        hata("403 Forbidden | Geçersiz işletim sistemi!")
+    li = os.getcwd().split(sep)
+    if pprint:rprint(li)
+    oathh="{}{}m-r".format(os.getcwd(),sep)
+    try:
+        os.makedirs(oathh)
+    except FileExistsError:
+        pass
+    return oathh
+
+async def getdirectory(oathh,file):
+    oathh="{main}{sep}{file}".format(main=oathh,sep=os.sep,file=file)
+    rprint(oathh)
+    return oathh
+async def setchannel(isp=0,pprint=True,forceadd=""):
+    global eklenecek; eklenecek=True
+    oathh=await setdirectory(pprint)
+
+    if isp == 0:
+                error=False
+                with open(await getdirectory(oathh,"main.txt"),"w") as f:
+                    if forceadd == "":
+                        neolsun=soru("🍀 Ana kanal ne olsun? Lütfen id'i yazın!")
+                        onayl = onay(f"Ana kanal '{neolsun}' olsun mu ?")
+                        try:
+                            neolsunn = int(neolsun)
+                        except ValueError:
+                            noadded("Lütfen bir kanal id yazın!");error=True
+                   
+                        if neolsun.startswith("-100") and onayl:
+                            f.write(neolsun);basarili("✅ İşlem başarıyla tamamlandı!")
+                        elif onayl==False:
+                            return await setchannel (isp, False, forceadd)
+                        else:
+                            log("Hatalı kanal id'si!","red");error=True
+                    else: f.write(adds+forceadd);basarili("✅ Force ({}) başarıyla tamamlandı!".format(forceadd))
+                if error:
+                    if os.path.isfile(await getdirectory(oathh,"main.txt")): os.remove(await getdirectory(oathh,"main.txt"))
+                    return await setchannel (isp, False, forceadd)
+                eklenecek=False
+                return await getdirectory(oathh,"main.txt")
+    elif isp == 1:
+                error=False
+                if os.path.isfile(await getdirectory(oathh,"channel.txt")):adds="\n"
+                else:adds=""
+                try:
+                    with open(await getdirectory(oathh,"channel.txt"),"r") as f:
+                        channelsss=f.read().split('\n')
+                except FileNotFoundError: channelsss=[]
+                with open(await getdirectory(oathh,"channel.txt"),"a") as f:
+                    if forceadd == "":
+                        neolsun=soru("🍀 Eklenecek yan kanal ne olsun? Lütfen id'i yazın!")
+                        onayl = onay(f"Yan kanallara '{neolsun}' eklensin mi ?")
+                        try:
+                            neolsunn = int(neolsun)
+                        except ValueError:
+                            noadded("Lütfen bir kanal id yazın!");error=True
+                        if neolsun in channelsss:noadded("Bu kadar zaten daha önceden eklenmiş!")
+                        elif neolsun.startswith("-100") and onayl:
+                            f.write(adds+neolsun);basarili("✅ İşlem başarıyla tamamlandı!")
+                        elif onayl==False:
+                            return await setchannel (isp,False, forceadd)
+                        else:
+                            log("Hatalı kanal id'si!","red");error=True
+                    else: f.write(adds+forceadd);basarili("✅ Force ({}) başarıyla tamamlandı!".format(forceadd))
+                if error:
+                    if os.path.isfile(await getdirectory(oathh,"channel.txt")): os.remove(await getdirectory(oathh,"channel.txt"))
+                    return await setchannel (isp, False, forceadd)
+                eklenecek=False
+                return await getdirectory(oathh,"channel.txt")
+
+async def getchannel (isp=0,pprint=True):
+    oathh=await setdirectory(pprint)
+    if isp == 0:
+                if os.path.isfile(await getdirectory(oathh,"main.txt")):
+                    with open(await getdirectory(oathh,"main.txt"),"r") as f:
+                        file = f.read()
+                    if not file.split('\n')[0].startswith("-100"):
+                        await setchannel (isp,False); return await getchannel (isp,False)
+                    return file.split('\n')[0]
+                else:
+                    await setchannel (isp,False); return await getchannel (isp,False)
+    elif isp == 1:
+                if os.path.isfile(await getdirectory(oathh,"channel.txt")):
+                    with open(await getdirectory(oathh,"channel.txt"),"r") as f:
+                        file = f.read()
+                    if not file.split('\n')[0].startswith("-100"):
+                        await setchannel (isp,False); return await getchannel (isp,False)
+                    return file.split('\n')
+                else:
+                    await setchannel (isp,False); return await getchannel (isp,False)
+    return None
+
+async def forchannel(bot,channelpath,message):
+    bilgi ("Perceived: ")
+    onemli(channelpath)
+    basarilic=0
+    mesj = await bot.get_messages(message.chat_id, ids=message.id)
+    bilgi("Kopyalanacak mesaj hazır!")
+    for chnl in channelpath:
+        if chnl == "":continue 
+        if chnl.startswith("-100"):
+            bilgi("Şuraya mesaj gönderilmeye çalışılıyor..: {}".format(chnl))
+            try:
+                chat=await bot.get_entity(int(chnl))
+                await bot.send_message(chat.id,mesj)
+                log("Mesaj {} kanalına gönderildi!".format(chat.id),"green")
+                basarilic+=1
+            except PeerIdInvalidError:
+                noadded("Kanal ID'si({}) hatalı, lütfen bunu silin!".format (chnl))
+            except Exception as e:
+                noadded("✖️ Yan kanallardan '{}' mesaj atılmadı! Hata: {}".format(chnl,str(e)))
+        else:
+            try:
+                chat=await bot.get_entity(int(chnl)) #types.PeerChannel(int(chnl))
+                await bot.send_message(chat,mesj)
+                log("Mesaj {} kanalına gönderildi!".format(chat.id),"green")
+                basarilic+=1
+            except Exception as e:
+                noadded("✖️ Yan kanallardan '{}' mesaj atılmadı! Hata: {}".format(chnl,str(e)))
+    return basarilic
+
+mainpath= ""
+channelpath=""
+async def main ():
+    statusz="ads"
+    if os.name!="nt": os.system("clear")
+    else: os.system("cls")
+    while True:
+        logo(True)
+        if statusz=="ads":ads("Free trial bitiş süresi: 31 gün",.5); statusz=None
+        elif statusz:
+            passed(statusz);statusz=None
+        passed("İşlemler:\n\n🍀 1:Botu başlat!\n🍀 2:Ana Kanal Ayarla veya Değiştir!\n🍀 3:Yan Kanal Ekle!\n🍀 4:Çıkış")
+        try:
+            islem = soru_("Yapacağınız işlemi seçin [1-4]?")
+        except:
+            await disconn ()
+        if islem=="1":
+            global bot, mainpath, channelpath 
+            mainpath= await getchannel (0)
+            channelpath= await getchannel (1)
+            bot = await botagir()
+            n()
+            log("💨💨 Şimdi botunuz çalışıyor ve ana kanalınızda birşey paylaşmanız bekleniyor...","green")
+            statusz="Bottan çıkış yapıldı!"
+            with console.status("[bold thistle1]⌛ Bot çalışıyor, durdurmak için Ctrl C yapın!") as status:
+                try:
+                    await bot.run_until_disconnected()
+                except KeyboardInterrupt:
+                    pass #raise KeyboardInterrupt("Çıkış!")
+            await disconn ()
+        elif islem=="2":
+            statusz="Ana kanal işlemlerinden çıkıldı!"
+            await setchannel ()
+            onayl = onay("Başka bir işlem yapmak ister misiniz?")
+            if onayl:continue
+            else:break
+        elif islem=="3":
+            statusz="Yan kanal işlemlerinden çıkıldı!"
+            await setchannel (1)
+            onayl = onay("Başka bir işlem yapmak ister misiniz?")
+            if onayl:continue
+            else: break
+        elif islem=="4":
+            break
+        if islem not in ["1","2","3","4"]:
+            statusz= "Hatalı işlem seçimi!"; continue 
+
+@clabtetikleyici(bot=bot,incoming=True, pattern="^.start",disable_edited=True)
+async def muutf(m):
+    await m.reply("Running...⚡")
+
+@clabtetikleyici(bot=bot,incoming=True, pattern="^.maingroup(?: |$)(.*)",disable_edited=True)
+async def muutf(m):
+    #string = m.pattern_match.group(1)
+    await m.reply("🆔: {}".format(mainpath))
+
+@clabtetikleyici(bot=bot,incoming=True, pattern="^.channels(?: |$)(.*)",disable_edited=True)
+async def muutf(m):
+    await m.respond("📋: {}".format(str(channelpath)))
+    text=""
+    for i in channelpath:
+        text+="🆔: {}\n".format(i)
+    await m.respond("{}".format(text))
+
+
+@clabtetikleyici(bot=bot,incoming=True,groups_only=True,disable_edited=True)
+async def muutf(m):
+    if str(m.chat_id) in channelpath:
+   
+        onemli("🔄 Yeni bir post tespit edildi,gönderiliyor...")
+
+        mesj = await bot.get_messages(m.chat_id, ids=m.id)
+        bilgi("Kopyalanacak mesaj hazır!")
+        try:
+            await bot.send_message(mainpath,mesj);onemli("✅ İşlem tamamlandı! Hedef post iletildi!")
+        except Exception as e:
+            noadded(f"{m.chat_id} kaynağından ana group hedefine iletilememe hatası: {str(e)}")
+
+        #else:
+        #await m.reply("✉️: {}".format(str(m)))
+     
+        
+    else:
+        bilgi(f"Şuradan bir mesaj algılandım🌀: {m.chat_id}")
+eklenecek=False
+
+@clabtetikleyici(bot=bot,incoming=True,groups_only=False,disable_edited=True,trigger_on_fwd=True)
+async def muutf(m):
+    if m.fwd_from and m.views:
+        await m.reply("🆔: <i>Kanal ID:</i> {}".format(m.fwd_from.from_id))
+
+
+"""
+@bot.on(bberc(incoming=True))
+async def handler(event):
+    await event.reply("b "+ event.text)
+"""
+async def disconn():
+    try:
+        await bot.disconnect()
+        log("Bottan çıkış yapıldı!","red")
+    except:
+        pass
+
+if __name__ == "__main__":
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        n()
+        loop.run_until_complete(disconn())
+        hata("Güle güle!")
+
+
+
+
+
+
